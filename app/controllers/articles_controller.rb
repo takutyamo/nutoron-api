4eb8125603997c101cf31921d4fc874f3b630fb3
takutyamo <takutyamo@gmail.com>
2022-01-16 11:34:04 +0900

    add cotorollers and routes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,17 @@
+class ArticlesController < ApplicationController
+
+    def index
+        puts 'index'
+        render json: {}
+    end
+
+    def create
+        puts 'create'
+        render json: {}
+    end
+
+    def show
+        puts 'show'
+        render json: {}
+    end
+end
